8cbb72a84634372f1032164f27bccf3648749403
cvivianc <viviuntiy@gmail.com>
2022-10-27 11:59:12 +0100

    commit

--- resources/js/code.js
+++ resources/js/code.js
@@ -15,6 +15,8 @@ var trials = 10;
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+
 ctx.beginPath();
 ctx.arc(100, 75, 50, 0, 2 * Math.PI);
 ctx.fillStyle = "#0e0370";
